7a14b182253e5bec23d989f53f7df11125541916
Bennett Hoffman <bennett@buttercoin.com>
2015-07-18 20:53:30 -0400

    Some dummy data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,13 @@
  * Controller of the podlyApp
  */
 angular.module('podlyApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope) {
+    $scope.episode = {
+      podcast: "Podlycast",
+      number: 1,
+      title: "The Poddening"
+    };
+
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
