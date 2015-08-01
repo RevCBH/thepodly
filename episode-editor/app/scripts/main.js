88ca7428054dfa6303f8a01751f5f30551c97076
Cedric Dahl <cedricdahl@gmail.com>
2015-08-01 15:00:48 -0700

    connected firebase, yo :D

--- episode-editor/app/scripts/main.js
+++ episode-editor/app/scripts/main.js
@@ -1,6 +1,11 @@
 // jshint devel:true
 
 $(document).ready(function() {
+
+  //create firebase reference
+  var url = 'sky-jump-run.firebaseIO.com'; 
+  var firebaseRef = new Firebase(url);
+
   // Setup Bootstrap tab handlers for the navigation pills in
   // the page header
   $('#header-nav li a').click(function (e) {
