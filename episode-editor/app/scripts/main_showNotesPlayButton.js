c8a58887c27d78eb331cb7d124b27ce4ab3c7865
Cedric Dahl <cedricdahl@gmail.com>
2015-08-23 05:18:57 -0700

    Editor Audio Controls
    Added play, pause, forward (5sec / 2sec) and back (5sec / 2sec).
    Controls are rough and time seeking is larger than 2/5 seconds but it’s
    a start.

--- episode-editor/app/scripts/main_showNotesPlayButton.js
+++ episode-editor/app/scripts/main_showNotesPlayButton.js
@@ -6,26 +6,63 @@
 $(document).ready(function() {
 	
 //initialize variables
-var audioPlayer = '<audio controls preload="load" id="audioPlayer"><source src="http://traffic.libsyn.com/healyourselfradio/biohacking_and_the_quantified_self.mp3" type="audio/mpeg"></audio>';
+var audioSource= 'http://traffic.libsyn.com/healyourselfradio/biohacking_and_the_quantified_self.mp3';
+var audioPlayer = '<audio controls preload="load" id="audioPlayer"><source src="' + audioSource + '" type="audio/mpeg"></audio>';
 
 
 
 
-//starting code
-	//write html into dom podcastAudioArea
-	//$('.podcastAudioArea').append('<button type="submit" class="btn btn-default" id="playButton_spewCount_000">Audio Play Test</button>');
-	$('.podcastAudioArea').append(audioPlayer);
-
-	//use event deligation to listen for this button being clicked and then activate
-	$('div').on('click', '#playButton_spewCount_1', function(){
-		//$('.podcastAudioArea').append("1");
-		var audio = $("#audioPlayer");
-         audio.trigger('play');
-        
-	});
+	//starting code
+		//write html into dom podcastAudioArea
+		//$('.podcastAudioArea').append('<button type="submit" class="btn btn-default" id="playButton_spewCount_000">Audio Play Test</button>');
+		$('.podcastAudioArea').append('<h4>Episode Media</h4>' + audioPlayer);
 
 
+	//Master Audio Controls
+		//play (use event deligation to listen for this button being clicked and then activate)
+		$('div').on('click', '#masterAudioControl_play', function(){
+			var audio = $("#audioPlayer");	
+	        audio.trigger('play'); 
+		});
+
+		//pause 
+		$('div').on('click', '#masterAudioControl_pause', function(){
+			var audio = $("#audioPlayer");
+	        audio.trigger('pause'); 
+		});
+
+		//back two sec
+		$('div').on('click', '#masterAudioControl_backTwoSec', function(){
+			var audio = $("#audioPlayer");
+	        audio.trigger('pause'); 
+	        audio.prop("currentTime",audio.prop("currentTime")-2);
+	        audio.trigger('play'); 
+		});
 	
+		
+		//back five sec
+		$('div').on('click', '#masterAudioControl_backFiveSec', function(){
+			var audio = $("#audioPlayer");
+	        audio.trigger('pause'); 
+	        audio.prop("currentTime",audio.prop("currentTime")-5);
+	        audio.trigger('play'); 
+		});
 
+		//forward two sec
+		$('div').on('click', '#masterAudioControl_forwardTwoSec', function(){
+			var audio = $("#audioPlayer");
+	        audio.trigger('pause'); 
+	        audio.prop("currentTime",audio.prop("currentTime")+2);
+	        audio.trigger('play'); 
+		});
+	
+		
+		//forward five sec
+		$('div').on('click', '#masterAudioControl_forwardiveSec', function(){
+			var audio = $("#audioPlayer");
+	        audio.trigger('pause'); 
+	        audio.prop("currentTime",audio.prop("currentTime")+5);
+	        audio.trigger('play'); 
+		});
 
 });		
