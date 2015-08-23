bc7ce2d9419037c4708790fdcc4bf31fd2951f3b
Cedric Dahl <cedricdahl@gmail.com>
2015-08-23 03:03:16 -0700

    Play Button
    Have audio loading and button controlling ‘play’ from
    main_showNotesPlayButton.js

--- episode-editor/app/scripts/main_showNotesPlayButton.js
+++ episode-editor/app/scripts/main_showNotesPlayButton.js
@@ -1,5 +1,4 @@
 // Test 4: hook up row play button to media O.O 
-	//play via button click 
 	//add media via FireBase
 	//make it play via 
 
@@ -7,17 +6,26 @@
 $(document).ready(function() {
 	
 //initialize variables
-var audio = new Audio('<audio controls preload="load"><source src="http://traffic.libsyn.com/healyourselfradio/biohacking_and_the_quantified_self.mp3" type="audio/mpeg"></audio>');
-audio.play();
+var audioPlayer = '<audio controls preload="load" id="audioPlayer"><source src="http://traffic.libsyn.com/healyourselfradio/biohacking_and_the_quantified_self.mp3" type="audio/mpeg"></audio>';
+
+
+
 
 //starting code
 	//write html into dom podcastAudioArea
-	$('.podcastAudioArea').append(audio + '<button type="submit" class="btn btn-default" id="playButton_spewCount_000">Audio Play Test</button>');
+	//$('.podcastAudioArea').append('<button type="submit" class="btn btn-default" id="playButton_spewCount_000">Audio Play Test</button>');
+	$('.podcastAudioArea').append(audioPlayer);
 
 	//use event deligation to listen for this button being clicked and then activate
 	$('div').on('click', '#playButton_spewCount_1', function(){
-		$('.podcastAudioArea').append("1");
-
+		//$('.podcastAudioArea').append("1");
+		var audio = $("#audioPlayer");
+         audio.trigger('play');
+        
 	});
+
+
+	
+
 
 });		
