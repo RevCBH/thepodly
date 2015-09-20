d2a11815e7ce08d683d4513f5e45a53968acee92
Bennett Hoffman <bennett@buttercoin.com>
2015-09-20 12:37:52 -0400

    Refactoring and comments

--- episode-editor/app/scripts/controllers/episodes.js
+++ episode-editor/app/scripts/controllers/episodes.js
@@ -1,76 +1,139 @@
 (function() {
   'use strict';
 
+  /* Convert the episodes object from firebase to the format we want and
+   * then sort them by episode number*/
+  function parseEpisodesObject(episodes) {
+    return _.chain(episodes)
+      .pairs() // this turns the object into an array of [id, data]
+      .map(function(x) { x[1]._id = x[0]; return x[1]; })
+      .sortBy(function(x) { return x.number; })
+      .value();
+  }
+
+  /* A podcast model 'class'. Given the id of a podcast, it will load the data
+   * for that podcast from firebase and trigger the updatedEvent whenever that
+   * data changes.
+   */
+  var Podcast = function(id) {
+    podly.Firebase.call(this);
+    this._id = id;
+    this.updatedEvent = new podly.Event('updated');
+
+    this.ref = this.firebase.child('podcasts/' + this._id);
+    this.ref.on('value', function(data) {
+      data = data.val();
+      this.title = data.title;
+      this.episodes = parseEpisodesObject(data.episodes);
+      this.updatedEvent.dispatch(this);
+    }.bind(this));
+  };
+
+  // Convenience method for adding a function to call when the podcast data
+  // updates.
+  Podcast.prototype.onUpdated = function(cb) {
+    this.updatedEvent.addCallback(cb);
+  };
+
+  /* Given an episode's id, delete it.
+   * When it actually gets deleted from firebase, it will end up triggering
+   * the updatedEvent.
+   * TODO - delete notes for that episode too!
+   */
+  Podcast.prototype.deleteEpisode = function(episodeId) {
+    this.ref.child('episodes/' + episodeId).remove();
+  };
+
+  /* A controller for the list of episodes in a podcast.
+   * It takes the AuthController as a construtor argument because it uses the
+   * account information to determine which podcast a user owns. The episode
+   * list and episode creation button won't show up until a user is logged in.
+   */
   var EpisodesController = function (AuthController) {
     podly.Controller.call(this, '#episode-list-panel');
     this.AuthController = AuthController;
-    this.episodes = undefined;
 
+    // When the user logs in, get their profile information and create a
+    // podcast object for their podcast.
     this.AuthController.onLoggedIn(function(authData) {
       var userRef = this.firebase.child('users/' + authData.uid);
       userRef.on('value', function(userData) {
         this.userData = userData.val();
-        var episodesRef = this.firebase.child('podcasts/' + this.userData.podcastId + '/episodes');
-        episodesRef.on('value', function(data) {
-          data = data.val();
-          for(var k in data) {
-            if(data.hasOwnProperty(k)) {
-              data[k]._id = k;
-            }
-          }
-          data = _.sortBy(data, function(x) { return x.number; });
-          console.log("updating episode data:", data);
-          this.updateEpisodes(data);
+        this.podcast = new Podcast(this.userData.podcastId);
+        this.podcast.onUpdated(function () {
+          this.updateView();
         }.bind(this));
       }.bind(this));
 
-      this.updateView(this.selectView());
+      this.updateView();
     }.bind(this));
 
+    // When the user logs out, update the view
+    this.AuthController.onLoggedOut(function() {
+      this.updateView();
+    }.bind(this));
+
+    // When we load the remote template, go to the current view
     this.templateLoaded.addCallback(function() {
-      this.updateView(this.selectView());
-
-      //var episodesRef = this.podcastRef.child('episodes');
-      //episodesRef.on('value', function(data) {
-        //this.updateEpisodes(data.val());
-      //}.bind(this));
-
-
-      this.AuthController.onLoggedOut(function() {
-        this.updateView(this.selectView());
-      }.bind(this));
+      this.updateView();
     }.bind(this));
   };
-
   EpisodesController.prototype = Object.create(podly.Controller.prototype);
 
-  EpisodesController.prototype.updateEpisodes = function (newEpisodes) {
-    this.episodes = newEpisodes;
-    this.updateView(this.selectView());
-  };
-
-  EpisodesController.prototype.selectView = function() {
-    if(this.templateSet === undefined) {
-      return $("<div></div>");
-    }
-
-    if(this.AuthController.isLoggedIn) {
-      if(this.episodes === undefined) {
-        return this.template('loading');
-      } else {
-        return this.wiredListView();
+  /* Chose the current thing to render and then render it. If a view is
+   * provided as an argument, assume that's the one to render.
+   *
+   * Otherwise:
+   *   - If our templates haven't loaded yet, render an empty div
+   *   - If we're not logged in, render the 'hidden' state
+   *   - If we don't have episodes loaded yet, render the 'loading' state
+   *   - Otherwise, render the episode list
+   *
+   * TODO - if the list of episodes updates while we're trying to create a new
+   *        episode, the control will switch back to the list view and any
+   *        pending changes will be lost!
+   */
+  EpisodesController.prototype.updateView = function(t) {
+    if(t === undefined) {
+      if(this.templateSet === undefined) {
+        t = $("<div></div>");
       }
-    } else {
-      return this.template('hidden');
+
+      if(this.AuthController.isLoggedIn) {
+        if(this.podcast === undefined || this.podcast.episodes === undefined) {
+          t = this.template('loading');
+        } else {
+          t = this.wireListView();
+        }
+      } else {
+        t = this.template('hidden');
+      }
     }
+    return podly.Controller.prototype.updateView.call(this, t);
   };
 
-  EpisodesController.prototype.wiredListView = function() {
+  /* Create an episode list view and wire up all of the event handlers for
+   * elements of the view. Specifically:
+   *   - the new post link should switch us to the form for a new episode
+   *   - the x delete links should delete the episode
+   *     TODO - have delete display a confirmation modal
+   *   - TODO - the episode title should load the appropriate episode in the
+   *            editor and probably collapse the episodes list (which would
+   *            be a new display state)
+   */
+  EpisodesController.prototype.wireListView = function() {
     var t = this.template('list');
+    var podcast = this.podcast;
     t.find('a#new-post-link').click(this.showNewEpisodeForm.bind(this));
+    t.find('a.delete-episode').click(function () {
+      var episodeId = $(this).attr('data-episodeId');
+      podcast.deleteEpisode(episodeId);
+      return false;
+    });
     return t;
   };
 
+  /* Wire events for and render the new episode form */
   EpisodesController.prototype.showNewEpisodeForm = function() {
     var t = this.template('new');
     t.find('form').submit(function () {
@@ -83,8 +146,11 @@
     }.bind(this));
 
     t.find('a#new-episode-cancel-button').click(function () {
-      this.updateView(this.selectView());
-      return false;
+      // TODO - we take advantage of the 'feature' here that the controller
+      // doesn't keep track of the fact we're showing the new episode form
+      // so if we have it updateView() automatically, it will go back to
+      // whatever state it thinks we should be in.
+      this.updateView();
     }.bind(this));
 
     this.updateView(t);
@@ -93,14 +159,12 @@
   podly.useTemplateFile('partials/episodes.html', EpisodesController);
 
   $(document).ready(function() {
+    // document.AuthController should be available before we try to setup the
+    // EpisodesController
     if(document.AuthController === undefined) {
       console.log("EpisodeController requires AuthController!");
     }
 
     document.EpisodesController = new EpisodesController(document.AuthController);
-
-    document.AuthController.onLoggedIn(function(authData) {
-    });
-
   });
 })();
--- episode-editor/app/scripts/lib/controller.js
+++ episode-editor/app/scripts/lib/controller.js
@@ -1,20 +1,47 @@
 (function() {
-  function Controller(panelId) {
-    this.panelId = panelId;
+  // A 'base class' for setting up something with a firebase connection
+  function WithFirebase() {
     this.firebase = new Firebase(Config.firebase.rootUrl);
   }
 
+  /* A 'base class' for Controllers
+   * That means:
+   *   1. It has a this.firebase property
+   *   2. It has a this.panelId property which is the jQuery selector it
+   *      uses to determine where on the page to render. For example:
+   *      if you this.panelId is "#some-elem-id" then the **contents** of
+   *      $("#some-elem-id") will be replace with whatever this controller
+   *      wants.
+   */
+  function Controller(panelId) {
+    WithFirebase.call(this);
+    this.panelId = panelId;
+  }
+  Controller.prototype = Object.create(WithFirebase.prototype);
+
+  /* A convenience function for setting the html of $(this.panelId) */
   Controller.prototype.updateView = function(view) {
     $(this.panelId).html(view);
   };
 
-  function useTemplateFile(templatePath, controllerClass) {
+  /* Setup a controller function to use a specific template file. It modifies
+   * the controller's prototype to add the template() method and the
+   * templateLoaded podly.Event which fires when the remote template file has
+   * been loaded.
+   *
+   * Paramters:
+   *    templatePath - a relative path to an .html file containing different
+   *                   controller templates
+   *    controllerFunction - a 'derived class' that implements Controller
+   */
+  function useTemplateFile(templatePath, controllerFunction) {
     (function() {
       this.renderTemplate = function(t) {
         return $(Handlebars.compile(t.prop('outerHTML'))(this));
       };
 
       this.templateLoaded = new podly.Event('templateLoaded');
+
       this.template = function(state) {
         var t = this.templateSet.find('[data-state=' + state + ']').clone();
 
@@ -25,9 +52,10 @@
         this.templateSet = $('<div>' + data + '</div>');
         this.templateLoaded.dispatch();
       }.bind(this));
-    }.bind(controllerClass.prototype))();
+    }.bind(controllerFunction.prototype))();
   }
 
   podly.useTemplateFile = useTemplateFile;
   podly.Controller = Controller;
+  podly.Firebase = WithFirebase;
 })();
