77d272ec237e3d3825c81e9db91c13bdc4fa667b
Cedric Dahl <cedricdahl@gmail.com>
2015-10-29 00:52:04 -0700

    Update Favicon & My local Short URL code

--- episode-editor/app/scripts/writeEmbedMagic.js
+++ episode-editor/app/scripts/writeEmbedMagic.js
@@ -317,9 +317,6 @@ $(document).ready(function(){
 // part 3: Display Episode Table from Firebase
 
 var displayEpisodeTable = function(baseUrl, ref2, hashtags){
-
-
-
     var episodeNotesRef = new Firebase(baseUrl + embedEpisodeNumber + '/episodeNotes');
 
     //Load the table data (middle of table) from firebase
@@ -396,8 +393,6 @@ var displayEpisodeTable = function(baseUrl, ref2, hashtags){
         //console.log(timeClean(2123));
         twttr.widgets.load();
       });
-
-
 
         //table close (bottom)
       $("#myTable").find('tbody').append($(tableBot));
